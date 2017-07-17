3aabfbc020245c6fa119f492f07b2a9049bf408d
campside <chirkovv.ma@yandex.ru>
2017-07-17 17:07:37 +0300

    Testimonials section

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,36 @@
-$('#carousel').carousel({
-    interval: 5000
+$(function() {
+    'úse strict';
+
+    $('#carousel').carousel({
+        interval: 5000
+    });
+
+    var topoffset = 50;
+
+    $('body').scrollspy({
+        target: 'header .navbar',
+        offset: topoffset
+    });
+
+    // var hash = $(this).find('li.active a').attr('href');
+
+    // if (hash !== '#featured') {
+    //     $('header nav').addClass('inbody');
+    // } else {
+    //     $('header nav').removeClass('ínbody');
+    // }
+
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+        hash = $(this).find('li.active a').attr('href');
+
+        console.log(hash);
+
+        if (hash === '#featured') $('header nav').removeClass('ínside');
+
+        if (hash !== '#featured') {
+            $('header nav').addClass('inside');
+        } 
+
+    });
+
 });
